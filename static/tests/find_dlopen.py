40b6d53e71f2f39507766c61c923aae32361b650
Knott Benoît <benoitknott060@gmail.com>
2023-09-12 11:41:59 +0200

    correct script for new system (as I've changed computers)

--- static/tests/find_dlopen.py
+++ static/tests/find_dlopen.py
@@ -3,13 +3,13 @@ import os
 
 for (root,dirs,files) in os.walk('/bin/',topdown=True):
     for f in files:
-        f_name = f"{root}/{f}"
+        f_name = f"{root}{f}"
 
         output = subprocess.run(["file", f_name], capture_output=True)
         str_output = output.stdout.decode("utf8")
         if str_output.split()[1] != "ELF":
             continue
-        
+
         output = subprocess.run(["objdump", "-d", "--disassembler-options=intel", f_name], capture_output=True)
 
         str_output = output.stdout.decode("utf8")
