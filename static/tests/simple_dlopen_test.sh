4c34d1a9089827853fe877e618bc474d0ed9080b
Knott Benoît <benoitknott060@gmail.com>
2023-09-12 14:57:59 +0200

    correct script for new system (as I've changed computers)

--- static/tests/simple_dlopen_test.sh
+++ static/tests/simple_dlopen_test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-n_lines=$(cat /home/ben/Documents/unif/github/impSyscall/code/analysis/static/tests/use_ldopen | grep "^/bin/" | wc -l)
+n_lines=$(cat /home/ben/Documents/unif/github/research/analysis/static/tests/use_dlopen | grep "^/bin/" | wc -l)
 i=1
-for line in $(cat /home/ben/Documents/unif/github/impSyscall/code/analysis/static/tests/use_ldopen | grep "^/bin/")
+for line in $(cat /home/ben/Documents/unif/github/research/analysis/static/tests/use_dlopen  | grep "^/bin/")
 do
     echo "$i/$n_lines: $line"
     timeout 30 python static_analyser.py --app $line -v f -d f --csv f 2> /dev/null
