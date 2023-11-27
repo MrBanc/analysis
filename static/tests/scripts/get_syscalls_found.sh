a555a3e9d54daf1c72fc430faf055c4e81ec10c8
Knott Benoît <bknott@uliege.be>
2023-11-27 11:15:28 +0100

    update script syscalls sinon on voit pas si le nb de syscalls a augmenté avec diff

--- static/tests/scripts/get_syscalls_found.sh
+++ static/tests/scripts/get_syscalls_found.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 cat /dev/null > /home/ben/Documents/unif/github/research/analysis/static/tests/data/nb_syscalls_found
+cat /dev/null > /home/ben/Documents/unif/github/research/analysis/static/tests/data/execution_time_syscalls_found
 n_lines=$(cat /home/ben/Documents/unif/github/research/analysis/static/tests/scripts/get_syscalls_found.sh | grep "^/bin/" | wc -l)
 i=1
 for line in $(cat /home/ben/Documents/unif/github/research/analysis/static/tests/scripts/get_syscalls_found.sh  | grep "^/bin/")
 do
     echo "$i/$n_lines: $line"
     result=$(/usr/bin/time -v timeout 600 python static_analyser.py --app $line -l f -v t -d f --csv f 2>&1 | sed -n -e '/^Total number of syscalls:/p' -e '/Elapsed (wall clock) time/p' | awk '{print $NF}' | awk 'BEGIN {RS=""; FS="\n"} { printf("%s\telapsed time: %s", $1, $2) }')
-    echo -e "$line: $result" >> /home/ben/Documents/unif/github/research/analysis/static/tests/data/nb_syscalls_found
+    echo -e "$line: $(echo $result | awk '{print $1}')" >> /home/ben/Documents/unif/github/research/analysis/static/tests/data/nb_syscalls_found
+    echo -e "$line: $(echo $result | awk '{print $2, $3, $4}')" >> /home/ben/Documents/unif/github/research/analysis/static/tests/data/execution_time_syscalls_found
     ((i++))
     sleep 1
 done
